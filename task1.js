db5ceff0134e20f15b82f52563cfdaf331e327df
vemuri15 <pavanivemuri2005@gmail.com>
2024-12-24 20:11:46 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,9 @@
+// Add 'scrolled' class to navbar when scrolling
+window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
